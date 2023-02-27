db3fbe257485e1d6270347ebdc388370ace82c64
Piotr Niewinski <p.niewinski@edrone.me>
2023-02-27 21:44:08 +0100

    initial commit after extract from pypaq

---
+++ r4c/policy_gradients/actor_critic_shared/ac_shared_actor.py
@@ -0,0 +1,41 @@
+from abc import ABC
+
+from typing import Optional, List, Dict, Any
+
+from pypaq.torchness.motorch import Module
+from r4c.helpers import extract_from_batch
+from r4c.policy_gradients.pg_actor import PGActor
+from r4c.policy_gradients.actor_critic_shared.ac_shared_actor_module import ACSharedActorModule
+
+
+class ACSharedActor(PGActor, ABC):
+
+    def __init__(
+            self,
+            name: str=                              'ACSharedActor',
+            module_type: Optional[type(Module)]=    ACSharedActorModule,
+            **kwargs):
+        PGActor.__init__(
+            self,
+            name=           name,
+            module_type=    module_type,
+            **kwargs)
+
+    def update_with_experience(
+            self,
+            batch: List[Dict[str, Any]],
+            inspect: bool
+    ) -> Dict[str, Any]:
+
+        observations = extract_from_batch(batch, 'dreturns')
+        obs_vecs = self._get_observation_vec_batch(observations)
+
+        out = self.model.backward(
+            observation=    obs_vecs,
+            action_taken=   extract_from_batch(batch, 'actions'),
+            qv_label=       extract_from_batch(batch, 'dreturns'))
+
+        out.pop('logits')
+        if 'probs' in out: out['probs'] = out['probs'].cpu().detach().numpy()
+
+        return out
---
+++ r4c/qlearning/qtable/qt_trainer.py
@@ -0,0 +1,26 @@
+"""
+
+ 2022 (c) piteren
+
+    QTableTrainer - Trainer specific for training of QTableActor, sets update_rate of Actor.
+
+"""
+
+from r4c.qlearning.ql_trainer import QLearningTrainer
+from r4c.qlearning.qtable.qt_actor import QTableActor
+
+
+class QTableTrainer(QLearningTrainer):
+
+    def __init__(
+            self,
+            actor: QTableActor,
+            update_rate: float,
+            **kwargs):
+
+        QLearningTrainer.__init__(self, actor=actor, **kwargs)
+        self.actor = actor
+        self.actor.set_update_rate(update_rate)
+
+        self._rlog.info('*** QTableTrainer *** initialized')
+        self._rlog.info('> actor update_rate: {update_rate}')
